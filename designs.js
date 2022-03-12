840c4e5ee008fd23961c9f3d756b54b5527f1663
JinaKim <gina7441@gmail.com>
2022-03-12 22:43:26 +0000

    refactor: simplify & cleanup the code

--- designs.js
+++ designs.js
@@ -1,29 +1,43 @@
+// global variables for accessing DOM elements.
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+const pixelCanvas = document.getElementById('pixelCanvas');
+const colorPicker = document.getElementById('colorPicker');
+// When size is submitted by the user, call makeGrid()
+const sizePicker = document.getElementById('sizePicker');
+sizePicker.addEventListener('submit', makeGrid);
+
+/**
+* @description Create a grid of squares
+* @param (event) - An object containing information about the action that just happened.
+*/
 function makeGrid(event) {
     // To prevent the page from redirecting which is default behaviour of
     // submit button inside the form element.
     event.preventDefault();
-    var height = document.getElementById('inputHeight').value;
-    var width = document.getElementById('inputWidth').value;
-    var new_canvas = document.getElementById('pixelCanvas');
-    var tableBody = document.createElement("tbody");
-    var new_color = document.getElementById('colorPicker');
-    new_color.addEventListener('input', ()=>{
-        var color = new_color.value;
-        console.log("The color you picked : "+color);
+
+    const tableBody = document.createElement('tbody');
+    let height = inputHeight.value;
+    let width = inputWidth.value;
+
+    colorPicker.addEventListener('input', () => {
+        let color = colorPicker.value;
+        console.log("The color you picked : " + color);
     });
 
     // The user can reset the grid to a blank state.
     let element = document.getElementById('pixelCanvas');
     // To remove all children from an element
-    while(element.firstChild){
+    while(element.firstChild) {
         element.removeChild(element.firstChild);
     }
 
-    for(var row = 0; row < width; row++){
-        var draw_row = document.createElement('tr');
+    // Create a grid of squares
+    for(let row = 0; row < height; row++) {
+        let draw_row = document.createElement('tr');
 
-        for(var col = 0; col < height; col++){
-            var draw_col = document.createElement('td');
+        for(let col = 0; col < width; col++) {
+            let draw_col = document.createElement('td');
             draw_row.appendChild(draw_col);
         }// inner for loop
 
@@ -33,18 +47,15 @@ function makeGrid(event) {
 
     // Each cell should have have an event listener
     // that sets the background color of the cell to the selected color.
-    new_canvas.addEventListener('click', function(event) {
+    pixelCanvas.addEventListener('click', function(event) {
         // Check if the clicked element is a cell (TD).
         // This way you just assign it once, since the table is not removed from DOM:
-        if(event.target.nodeName === 'TD'){
-            event.target.style.backgroundColor = new_color.value;
+        if(event.target.nodeName === 'TD') {
+            let color = colorPicker.value;
+            event.target.style.backgroundColor = color;
         }
     })
 
     // add the table body in the table
-    new_canvas.appendChild(tableBody);
+    pixelCanvas.appendChild(tableBody);
 }
-
-// When size is submitted by the user, call makeGrid()
-var new_size = document.getElementById('sizePicker');
-new_size.addEventListener('submit', makeGrid);
